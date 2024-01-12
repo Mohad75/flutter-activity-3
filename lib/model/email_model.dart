eba675d4c15e050b8689949c1cfa508761fd505b
Matanog <matanogmm@students.national-u.edu.ph>
2024-01-12 09:58:29 +0800

    first

---
+++ lib/model/email_model.dart
@@ -0,0 +1,18 @@
+class Email {
+  const Email({
+    required this.sender,
+    required this.time,
+    required this.subject,
+    required this.message,
+    required this.avatar,
+    
+    required this.containsPictures,
+  });
+
+  final String sender;
+  final String time;
+  final String subject;
+  final String message;
+  final String avatar;
+  final bool containsPictures;
+}
